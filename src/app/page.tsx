cee4efa8de2865d8e47ce3b217490e8ba643377d
Pavel Kocman <kocmanpavel@gmail.com>
2025-06-25 08:45:03 +0200

    feat(auth): Add auth middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-"use client";
+import { authServer } from "@/auth/auth-server";
+import { headers } from "next/headers";
 
-export default function Home() {
-  return <div className="flex flex-1 items-center justify-center">Home</div>;
+export default async function Home() {
+  const auth = await authServer.api.getSession({
+    headers: await headers(),
+  });
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      {auth?.user.email}
+    </div>
+  );
 }
--- src/auth/auth-server.ts
+++ src/auth/auth-server.ts
@@ -3,3 +3,5 @@ import { betterAuth, BetterAuthOptions } from "better-auth";
 import { authConfig } from "./auth.config";
 
 export const authServer = betterAuth(authConfig);
+
+export type Session = typeof authServer.$Infer.Session;
