432c864f45d77e6d7f0a6b99a83031a4d4644da1
Pavel Kocman <kocmanpavel@gmail.com>
2026-03-26 12:48:25 +0100

    chire: bump up Nextjs version

--- src/auth/auth-client.ts
+++ src/auth/auth-client.ts
@@ -1,7 +1,9 @@
+"use client";
+
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
   plugins: [adminClient()],
 });
